38f81d8d364443880592d81cf03373856cfece09
LucXiong <55743681+LucXiong@users.noreply.github.com>
2021-12-09 16:23:26 +0800

    Update CSA.py

--- CSA.py
+++ CSA.py
@@ -100,7 +100,7 @@ class CSA():
 def demo_func(x):
     x1 = x[0]
     x2 = x[1]
-    result = 4 * x1 ** - 2.1 * x1 ** 4 + x1 ** 6 /3 + x1 * x2 - 4 * x2 ** 2 + 4 * x2 ** 4
+    result = 4 * x1 ** 2 - 2.1 * x1 ** 4 + x1 ** 6 /3 + x1 * x2 - 4 * x2 ** 2 + 4 * x2 ** 4
     return result
 
 if __name__ == '__main__':
